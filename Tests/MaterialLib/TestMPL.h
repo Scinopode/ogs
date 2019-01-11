08302a4b7d1de846493892eda808fd86e920330e
Norbert Grunwald <n.g.boettcher@gmail.com>
2019-01-11 11:57:16 +0100

    added TestMPL header

---
+++ Tests/MaterialLib/TestMPL.h
@@ -0,0 +1,24 @@
+/**
+ * \author Norbert Grunwald
+ * \date   Oct 22, 2018
+ * \brief
+ *
+ * \copyright
+ * Copyright (c) 2012-2018, OpenGeoSys Community (http://www.opengeosys.org)
+ *            Distributed under a Modified BSD License.
+ *              See accompanying file LICENSE.txt or
+ *              http://www.opengeosys.org/project/license
+ *
+ */
+
+#pragma once
+
+//#include <gtest/gtest.h>
+//#include <memory>
+
+#include "Tests/TestTools.h"
+
+#include "MaterialLib/MPL/mpMedium.h"
+namespace MPL = MaterialPropertyLib;
+
+MPL::Medium createTestMaterial(std::string const& xml);
